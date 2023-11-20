1d5a69b2fd145014dcf3990bb4f5cfbd0bfa5fa3
Chirag <chirag389817@gmail.com>
2023-11-20 13:28:12 +0530

    add patient, admit and temperature - chirag

---
+++ src/modals/Admit.js
@@ -0,0 +1,36 @@
+const { AutoIncrementID } = require("@typegoose/auto-increment");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const admitSchema = new mongoose.Schema({
+    _id: Number,
+    patientId: {
+        type: Number,
+        ref: "Patient"
+    },
+    admitDate: {
+        type: Date,
+        default: Date.now()
+    },
+    dischargeDate: {
+        type: Date,
+        required: false
+    },
+    isDischarged: {
+        type: "boolean",
+        default: false
+    },
+    temperatures: [
+        {
+            date: Date,
+            time: String,
+            temp: Number
+        }
+    ]
+});
+
+admitSchema.plugin(AutoIncrementID, {});
+
+const Admit = mongoose.model("Admit", admitSchema);
+
+module.exports = Admit;
